8f88d5fc25bea688b59d96f1ada03d4ea5542337
Pantelis Stampoulis <pantelis.stampoulis@gmail.com>
2018-09-26 17:13:40 +0300

    add form for messages and show incoming messages

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ const http = require('http');
 const path = require('path');
 const publicPath = path.join(__dirname + '/../public');
 const port = process.env.PORT || 3000;
+const messageUtils = require('./utils/message');
 
 var app = express();
 var server = http.createServer(app);
@@ -14,33 +15,20 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
   console.log('New User connected');
 
-  socket.emit('newMessage', {
-    from: 'Admin',
-    text: 'Welcome to the chat app',
-    createdAt: new Date().getTime()
-  });
+  socket.emit('newMessage', messageUtils.generateMessage('Admin','Welcome to the chat app'));
+  socket.broadcast.emit('newMessage', messageUtils.generateMessage('Admin','New user joined'));
 
-  socket.broadcast.emit('newMessage', {
-    from: 'Admin',
-    text: 'New user joined',
-    createdAt: new Date().getTime()
-  });
 
   socket.on('disconnect', () => {
     console.log('User diconnected from server');
   });
 
   //edw tha lavei o server event apo to client
-  socket.on('createMessage', (message) => {
+  socket.on('createMessage', (message, callback) => {
     console.log('createMessage', message);
 
-    io.emit('newMessage', {
-      from: message.from,
-      text: message.text,
-      createdAt: new Date().getTime()
-    });
-
-
+    io.emit('newMessage', messageUtils.generateMessage(message.from,message.text));
+    callback('This is from the server');
     /*
     socket.broadcast.emit('newMessage', {
       from: message.from,
