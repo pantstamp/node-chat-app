8e6d0b3ecbffb049e8ad662c46e37cbea4f4b39b
Pantelis Stampoulis <pantelis.stampoulis@gmail.com>
2018-09-26 14:48:53 +0300

    Greet new user and alert others

--- server/server.js
+++ server/server.js
@@ -14,6 +14,18 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
   console.log('New User connected');
 
+  socket.emit('newMessage', {
+    from: 'Admin',
+    text: 'Welcome to the chat app',
+    createdAt: new Date().getTime()
+  });
+
+  socket.broadcast.emit('newMessage', {
+    from: 'Admin',
+    text: 'New user joined',
+    createdAt: new Date().getTime()
+  });
+
   socket.on('disconnect', () => {
     console.log('User diconnected from server');
   });
@@ -21,11 +33,21 @@ io.on('connection', (socket) => {
   //edw tha lavei o server event apo to client
   socket.on('createMessage', (message) => {
     console.log('createMessage', message);
+
     io.emit('newMessage', {
       from: message.from,
       text: message.text,
       createdAt: new Date().getTime()
     });
+
+
+    /*
+    socket.broadcast.emit('newMessage', {
+      from: message.from,
+      text: message.text,
+      createdAt: new Date().getTime()
+    });
+    */
   });
 
 });
