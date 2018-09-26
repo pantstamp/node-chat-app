b8133eebde75d53564b5ad7f5dc7c38dd8491bab
Pantelis Stampoulis <pantelis.stampoulis@gmail.com>
2018-09-26 14:30:55 +0300

    Emit message on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -2,11 +2,6 @@ var socket = io();
 
 socket.on('connect', function() {
   console.log('Connected to server');
-
-  socket.emit('createMessage', {
-    from: 'mike',
-    text: 'Hey!!! How are you?'
-  });
 });
 
 socket.on('disconnect', function() {
--- server/server.js
+++ server/server.js
@@ -14,19 +14,18 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
   console.log('New User connected');
 
-  socket.emit('newMessage', {
-    from: 'mike',
-    text: 'Hello!!!',
-    createdAt: 123
-  });
-
   socket.on('disconnect', () => {
     console.log('User diconnected from server');
   });
 
   //edw tha lavei o server event apo to client
-  socket.on('createMessage', (newMessage) => {
-    console.log('createMessage', newMessage);
+  socket.on('createMessage', (message) => {
+    console.log('createMessage', message);
+    io.emit('newMessage', {
+      from: message.from,
+      text: message.text,
+      createdAt: new Date().getTime()
+    });
   });
 
 });
